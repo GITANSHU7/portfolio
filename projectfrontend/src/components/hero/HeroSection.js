8bef2706bd979706a6f21601c4aaf47858611f0c
GITANSHU7 <gitanshugautam7@gmail.com>
2021-04-16 10:44:48 +0530

    icons

--- projectfrontend/src/components/hero/HeroSection.js
+++ projectfrontend/src/components/hero/HeroSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { RiInstagramFill } from 'react-icons/ri';
+import { ImLinkedin } from 'react-icons/im';
 import './HeroSection.css';
 
 
@@ -11,8 +12,11 @@ function HeroSection() {
       <video src='/videos/select.mp4' autoPlay loop muted />
      
      <h1 >Hi , I am  Gitanshu Gautam</h1>
-     
-     
+      <i className="fab fa-github" style={{backgroundColor : "white" , fontSize : "50px" , borderRadius : "50%" ,}}></i>
+      <i className="fab fa-linkedin" style={{backgroundColor : "white" , fontSize : "50px"  , display : "flex"}}></i>
+      <i className="fab fa-instagram-square"  style={{backgroundColor : "white" , fontSize : "50px"  , borderRadius : "50%", display : "flex"}}></i>
+     <RiInstagramFill  style={{backgroundColor : "white" , fontSize : "50px"  , borderRadius : "50%", display : "flex"}}/>
+     <ImLinkedin   style={{backgroundColor : "white" , fontSize : "50px"  , borderRadius : "50%", display : "flex"}}/>
       
   
     </div>
