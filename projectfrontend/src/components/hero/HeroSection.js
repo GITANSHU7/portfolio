3b0056df701e5c5def288aa19e5c5e2459256d76
GITANSHU7 <gitanshugautam7@gmail.com>
2021-04-16 21:43:27 +0530

    type effct

--- projectfrontend/src/components/hero/HeroSection.js
+++ projectfrontend/src/components/hero/HeroSection.js
@@ -2,8 +2,36 @@ import React from 'react';
 import { RiInstagramFill } from 'react-icons/ri';
 import { ImLinkedin } from 'react-icons/im';
 import './HeroSection.css';
+import styled from "styled-components";
+import Typical from 'react-typical'
 
+const SocialContainer = styled.div`
+  display: flex;
+`;
 
+const TheEffect = styled.div`
+
+`;
+
+const SocialIcon = styled.div`
+  color: #fff;
+  font-size: 45px;
+  
+  transition: all 200ms ease-in-out;
+  cursor: pointer;
+  &:not(:last-of-type) {
+    margin-right: 11px;
+    @media screen and (max-width: 480px) {
+      margin-right: 9px;
+    }
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 25px;
+  }
+  &:hover {
+    color: #adadad;
+  }
+`;
 
 
 function HeroSection() {
@@ -12,12 +40,28 @@ function HeroSection() {
       <video src='/videos/select.mp4' autoPlay loop muted />
      
      <h1 >Hi , I am  Gitanshu Gautam</h1>
-      <i className="fab fa-github" style={{backgroundColor : "white" , fontSize : "50px" , borderRadius : "50%" ,}}></i>
-      <i className="fab fa-linkedin" style={{backgroundColor : "white" , fontSize : "50px"  , display : "flex"}}></i>
-      <i className="fab fa-instagram-square"  style={{backgroundColor : "white" , fontSize : "50px"  , borderRadius : "50%", display : "flex"}}></i>
-     <RiInstagramFill  style={{backgroundColor : "white" , fontSize : "50px"  , borderRadius : "50%", display : "flex"}}/>
-     <ImLinkedin   style={{backgroundColor : "white" , fontSize : "50px"  , borderRadius : "50%", display : "flex"}}/>
-      
+     <br />
+     <p>Hello 👋 , I'm a <Typical
+        steps={[ 'Developer', 1200 , 'Cricketer' , 1200 , 'WordPress Developer' , 1200]}
+        loop={Infinity}
+        wrapper="b"
+      /> </p>
+      <br />
+     <SocialContainer>
+          <SocialIcon>
+          <i className="fab fa-twitter"></i>
+          </SocialIcon>
+          <SocialIcon>
+          <i className="fab fa-linkedin-in"></i>
+          </SocialIcon>
+          <SocialIcon>
+          <i className="fab fa-instagram"></i>
+          </SocialIcon><SocialIcon>
+          <i className="fab fa-github"></i>
+          </SocialIcon>
+
+        </SocialContainer>
+    
   
     </div>
   );
