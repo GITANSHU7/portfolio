b88055f3073405cf6fab61c83e280c78cfe18b6d
GITANSHU7 <gitanshugautam7@gmail.com>
2021-05-24 22:13:04 +0530

    progress bar

--- projectfrontend/src/core/Home.js
+++ projectfrontend/src/core/Home.js
@@ -4,6 +4,7 @@ import { API } from "../backend";
 import AdminDashBoard from "../user/AdminDashBoard";
 import Cart from "./Cart";
 import HeroSection from "../components/hero/HeroSection";
+import Skill from "../components/SkillSetup/UI/Skill"
 
 
 
@@ -14,6 +15,9 @@ export default function Home() {
   return (
     <>
     <HeroSection />
+    <div style = {{backgroundColor  : "white"}}>
+    <Skill />
+    </div>
         </>
   );
 }
